e0516e51f08bab9c48bf2361d70f8cab5ee3b76c
agiledon <bruce.e.zhang@gmail.com>
2013-03-27 14:39:27 +0800

    [Yi]add the MatchService for smart-match

---
+++ smart-match/src/main/java/com/github/smart/match/service/MatchService.java
@@ -0,0 +1,11 @@
+package com.github.smart.match.service;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Zhang Yi
+ * Date: 3/27/13
+ * Time: 2:36 PM
+ */
+public interface MatchService {
+    boolean matchIndividual(int firstProfileId, int secondProfileId);
+}
