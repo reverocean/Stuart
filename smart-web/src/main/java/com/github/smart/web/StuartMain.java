1064ee5337d05cd349600be943a1769605472c5d
dreamhead <dreamhead.cn@gmail.com>
2013-03-28 07:03:21 +0800

    added match job resource

--- smart-web/src/main/java/com/github/smart/web/StuartMain.java
+++ smart-web/src/main/java/com/github/smart/web/StuartMain.java
@@ -2,20 +2,34 @@ package com.github.smart.web;
 
 import com.bazaarvoice.dropwizard.assets.ConfiguredAssetsBundle;
 import com.github.smart.domain.Customer;
+import com.github.smart.domain.Profile;
+import com.github.smart.match.job.CustomerWriter;
 import com.github.smart.recommendation.RecommendationService;
 import com.github.smart.service.DefaultLessThanService;
 import com.github.smart.service.DefaultRecommendationService;
 import com.github.smart.web.config.StuartConfiguration;
 import com.github.smart.web.resources.*;
+import com.google.common.collect.ImmutableList;
 import com.yammer.dropwizard.Service;
 import com.yammer.dropwizard.config.Bootstrap;
 import com.yammer.dropwizard.config.Environment;
 import com.yammer.dropwizard.db.DatabaseConfiguration;
 import com.yammer.dropwizard.hibernate.HibernateBundle;
 import com.yammer.dropwizard.views.ViewBundle;
+import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.SimpleJob;
 import org.springframework.batch.core.launch.JobLauncher;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
+import org.springframework.batch.core.step.item.ChunkOrientedTasklet;
+import org.springframework.batch.core.step.item.SimpleChunkProcessor;
+import org.springframework.batch.core.step.item.SimpleChunkProvider;
+import org.springframework.batch.core.step.tasklet.TaskletStep;
+import org.springframework.batch.item.database.HibernateCursorItemReader;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 public class StuartMain extends Service<StuartConfiguration> {
     private final HibernateBundle<StuartConfiguration> hibernate = new HibernateBundle<StuartConfiguration>(Customer.class) {
@@ -44,14 +58,63 @@ public class StuartMain extends Service<StuartConfiguration> {
         environment.addResource(createRecommendationResource());
         environment.addResource(new RecommendResource());
 
-//        environment.addResource(createMatchJobResource());
+        environment.addResource(createMatchJobResource(configuration));
     }
 
-    private MatchJobResource createMatchJobResource() {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("matchJobApplicationContext.xml");
-        JobLauncher jobLauncher = context.getBean("jobLauncher", JobLauncher.class);
-        Job job = context.getBean("matchJob", Job.class);
+    private MatchJobResource createMatchJobResource(StuartConfiguration configuration) {
+        Job job = createJob();
+        JobLauncher jobLauncher = createJobLauncher(configuration);
         return new MatchJobResource(jobLauncher, job);
+
+
+//        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("matchJobApplicationContext.xml");
+//        JobLauncher jobLauncher = context.getBean("jobLauncher", JobLauncher.class);
+//        Job job = context.getBean("matchJob", Job.class);
+//        return new MatchJobResource(jobLauncher, job);
+    }
+
+    private SimpleJob createJob() {
+        SimpleJob job = new SimpleJob();
+        job.setName("MatchJob");
+        TaskletStep taskletStep = new TaskletStep();
+        HibernateCursorItemReader itemReader = new HibernateCursorItemReader();
+        itemReader.setSessionFactory(hibernate.getSessionFactory());
+        SimpleChunkProvider<Profile> chunkProvider = new SimpleChunkProvider<Profile>(itemReader, null);
+        SimpleChunkProcessor<Profile, Profile> chunkProcessor = new SimpleChunkProcessor<Profile, Profile>(null, new CustomerWriter());
+        ChunkOrientedTasklet<Profile> tasklet = new ChunkOrientedTasklet<Profile>(chunkProvider, chunkProcessor);
+        taskletStep.setTasklet(tasklet);
+        Step step = taskletStep;
+        job.setSteps(ImmutableList.of(step));
+        return job;
+    }
+
+    private SimpleJobLauncher createJobLauncher(StuartConfiguration configuration) {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        JobRepositoryFactoryBean jobRepositoryFactoryBean = new JobRepositoryFactoryBean();
+        BasicDataSource dataSource = createDataSource(configuration);
+        jobRepositoryFactoryBean.setDataSource(dataSource);
+        DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
+        transactionManager.setDataSource(dataSource);
+        jobRepositoryFactoryBean.setTransactionManager(transactionManager);
+
+        JobRepository jobRepository = null;
+        try {
+            jobRepository = (JobRepository)jobRepositoryFactoryBean.getObject();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        jobLauncher.setJobRepository(jobRepository);
+        return jobLauncher;
+    }
+
+    private BasicDataSource createDataSource(StuartConfiguration configuration) {
+        BasicDataSource dataSource = new BasicDataSource();
+        DatabaseConfiguration databaseConfiguration = configuration.getDatabaseConfiguration();
+        dataSource.setDriverClassName(databaseConfiguration.getDriverClass());
+        dataSource.setUrl(databaseConfiguration.getUrl());
+        dataSource.setUsername(databaseConfiguration.getUser());
+        dataSource.setPassword(databaseConfiguration.getPassword());
+        return dataSource;
     }
 
     private RecommendationResource createRecommendationResource() {
--- smart-web/src/main/java/com/github/smart/web/resources/MatchJobResource.java
+++ smart-web/src/main/java/com/github/smart/web/resources/MatchJobResource.java
@@ -1,8 +1,12 @@
 package com.github.smart.web.resources;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
@@ -10,6 +14,7 @@ import javax.ws.rs.core.MediaType;
 @Path("/matchjob")
 @Produces(MediaType.TEXT_PLAIN)
 public class MatchJobResource {
+    private Logger logger = LoggerFactory.getLogger(MatchJobResource.class);
     private JobLauncher launcher;
     private final Job job;
 
@@ -18,11 +23,13 @@ public class MatchJobResource {
         this.job = job;
     }
 
+    @GET
     public String matchJob() {
         try {
-            launcher.run(this.job, null);
+            launcher.run(this.job, new JobParameters());
             return "Succeed to run job";
         } catch (Exception e) {
+            logger.error("fail to run job", e);
             return "Fail to run job";
         }
     }
