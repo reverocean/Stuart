46c4f9edf53c8b83f427b0b78afde21ce2db4aeb
Davenkin <davenkin@163.com>
2013-03-27 15:29:14 +0800

    added customer interface

---
+++ smart-persistence/src/main/java/smart/domain/Customer.java
@@ -0,0 +1,20 @@
+package smart.domain;
+
+public class Customer
+{
+    private Address address;
+
+    public Customer()
+    {
+    }
+
+    public Address getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(Address address)
+    {
+        this.address = address;
+    }
+}
---
+++ smart-persistence/src/main/java/smart/service/CustomerService.java
@@ -0,0 +1,9 @@
+package smart.service;
+
+import smart.domain.Customer;
+
+public interface CustomerService
+{
+    public Customer findById(int customerId);
+
+}
