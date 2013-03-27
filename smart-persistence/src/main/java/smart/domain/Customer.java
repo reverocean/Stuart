86cf9588ac136667b5b558e1ab32b02819495bdb
Davenkin <davenkin@163.com>
2013-03-27 15:34:08 +0800

    refined Customer

--- smart-persistence/src/main/java/smart/domain/Customer.java
+++ smart-persistence/src/main/java/smart/domain/Customer.java
@@ -1,7 +1,17 @@
 package smart.domain;
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
 public class Customer
 {
+    private int id;
+    private String name;
+    private String email;
+    private String brand;
+    private Date dateOfBirth;
+    private String gender;
+    private Timestamp registerTime;
     private Address address;
 
     public Customer()
@@ -16,5 +26,75 @@ public class Customer
     public void setAddress(Address address)
     {
         this.address = address;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public String getEmail()
+    {
+        return email;
+    }
+
+    public void setEmail(String email)
+    {
+        this.email = email;
+    }
+
+    public String getBrand()
+    {
+        return brand;
+    }
+
+    public void setBrand(String brand)
+    {
+        this.brand = brand;
+    }
+
+    public Date getDateOfBirth()
+    {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth)
+    {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getGender()
+    {
+        return gender;
+    }
+
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+
+    public Timestamp getRegisterTime()
+    {
+        return registerTime;
+    }
+
+    public void setRegisterTime(Timestamp registerTime)
+    {
+        this.registerTime = registerTime;
     }
 }
