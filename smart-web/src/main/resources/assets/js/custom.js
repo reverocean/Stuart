d45b869a051772035de84442e95bdcff68b0ebb8
Haiyang He <hyhe@thoughtworks.com>
2013-03-27 17:21:11 +0800

    [yu yang | haiyang] add css and javascript for Stuart index page

---
+++ smart-web/src/main/resources/assets/js/custom.js
@@ -0,0 +1,28 @@
+$(function(){
+	$("#accordion").accordion({
+		heightStyle: "content"
+	});
+
+	$("#dialog").dialog({
+      autoOpen: false,
+      height: 300,
+      width: 480,
+      modal: true,
+      show: {
+        effect: "blind",
+        duration: 1000
+      },
+      hide: {
+        effect: "blind",
+        duration: 1000
+      }
+    });
+
+	$(".rec a.name").each(function() {
+		$(this).click(function() {
+	      		
+	      $("#dialog").dialog("open");
+	    });
+	});
+});
+
