14de20646dc44fe252ca92f7e82cc635a50d1e42
Davenkin <davenkin@163.com>
2013-03-28 08:51:16 +0800

    added less than service

--- smart-persistence/src/main/java/com/github/smart/service/DefaultLessThanService.java
+++ smart-persistence/src/main/java/com/github/smart/service/DefaultLessThanService.java
@@ -1,21 +1,30 @@
 package com.github.smart.service;
 
 import com.github.smart.domain.Customer;
+import org.hibernate.Query;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Required;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public class DefaultLessThanService implements LessThanService {
+public class DefaultLessThanService implements LessThanService
+{
     private SessionFactory sessionFactory;
 
     @Override
-    public List<Customer> getLessBrandsCustomers(int limit) {
-        return null;
+    @Transactional
+    public List<Customer> getLessBrandsCustomers(int limit)
+    {
+        Session currentSession = sessionFactory.getCurrentSession();
+        Query query = currentSession.createQuery("FROM Customer c WHERE size(c.profiles) < " + limit);
+        return query.list();
     }
 
     @Required
-    public void setSessionFactory(SessionFactory sessionFactory) {
+    public void setSessionFactory(SessionFactory sessionFactory)
+    {
         this.sessionFactory = sessionFactory;
     }
 }
--- smart-persistence/src/test/java/com/github/smart/service/DefaultCustomerServiceTest.java
+++ smart-persistence/src/test/java/com/github/smart/service/DefaultCustomerServiceTest.java
@@ -1,5 +1,9 @@
 package com.github.smart.service;
 
+import com.github.smart.domain.Address;
+import com.github.smart.domain.Customer;
+import com.github.smart.domain.Individual;
+import com.github.smart.domain.Profile;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +14,11 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 
+import java.sql.Date;
+import java.sql.Timestamp;
 import java.util.List;
+
+import static com.google.common.collect.Sets.newHashSet;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:testHibernateApplicationContext.xml"})
@@ -25,6 +33,9 @@ public class DefaultCustomerServiceTest
     @Autowired
     RecommendationService recommendationService;
 
+    @Autowired
+    LessThanService lessThanService;
+
     @Test
     public void test(){
 //        defaultCustomerService.saveIndividual();
@@ -32,13 +43,38 @@ public class DefaultCustomerServiceTest
 //        defaultCustomerService.saveIndividual("aami");
 //        defaultCustomerService.saveIndividual("bingle");
         recommendationService.saveSimilarity("suncorp", "bingle", 12.0);
-//        defaultCustomerService.createCustomer();
+        Customer customer = new Customer();
+        customer.setName("davenkin");
+        customer.setProfiles(newHashSet(createProfile("suncorp"),createProfile("bingle")));
+        defaultCustomerService.save(customer);
         List<String> strings = recommendationService.retrieveBrands();
         Double aDouble = recommendationService.retrieveSimilarity("suncorp", "bingle");
         List<String> brands = recommendationService.findCustomerBrands(1);
+        List<Customer> lessBrandsCustomers = lessThanService.getLessBrandsCustomers(2);
         System.out.println("dd");
         System.out.println(strings.size());
         System.out.println(aDouble);
         System.out.println(brands.get(0));
+        System.out.println(lessBrandsCustomers.size());
+    }
+
+    private Profile createProfile(String brand)
+    {
+        Individual individual = new Individual();
+        individual.setName("name");
+        Address address = new Address();
+        address.setAddressLine("line");
+        address.setCity("hechuan");
+        address.setPostCode("12345");
+        address.setState("chongqing");
+        address.setStreet("tianfu");
+        individual.setAddress(address);
+        individual.setEmail("davenkin");
+        individual.setDateOfBirth(new Date(System.currentTimeMillis()));
+        individual.setRegisterTime(new Timestamp(System.currentTimeMillis()));
+        Profile profile = new Profile();
+        profile.setBrand(brand);
+        profile.setIndividual(individual);
+        return profile;
     }
 }
