641dc920dea69b144fd2c847d6c1bc911f148e3d
agiledon <bruce.e.zhang@gmail.com>
2013-03-28 09:50:28 +0800

    [Yi]add the name perfect match

--- smart-match/src/main/java/com/github/smart/match/strategy/PerfectMatchStrategy.java
+++ smart-match/src/main/java/com/github/smart/match/strategy/PerfectMatchStrategy.java
@@ -7,9 +7,21 @@ import com.github.smart.domain.Customer;
 public class PerfectMatchStrategy implements MatchStrategy {
     @Override
     public Similarity match(Profile firstProfile, Profile secondProfile) {
+        if (perfectMatchName(firstProfile, secondProfile)) return new Similarity(0);
         int total = firstProfile.totalFields();
         int similarity = firstProfile.compare(secondProfile);
         return new Similarity(getSimilarityPercentage(similarity, total));
+    }
+
+    private boolean perfectMatchName(Profile firstProfile, Profile secondProfile) {
+        String firstCustomerName = firstProfile.getIndividual().getName();
+        String secondCustomerName = secondProfile.getIndividual().getName();
+        if (firstCustomerName != null && secondCustomerName != null) {
+            if (firstCustomerName.equalsIgnoreCase(secondCustomerName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private int getSimilarityPercentage(int similarity, int total) {
