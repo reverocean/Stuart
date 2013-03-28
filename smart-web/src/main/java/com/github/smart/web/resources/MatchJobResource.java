b87f5294df6d265c5514f0bff52b21720a69bb9a
dreamhead <dreamhead.cn@gmail.com>
2013-03-28 09:48:18 +0800

    connected to matchjob table

--- smart-web/src/main/java/com/github/smart/web/resources/MatchJobResource.java
+++ smart-web/src/main/java/com/github/smart/web/resources/MatchJobResource.java
@@ -3,6 +3,7 @@ package com.github.smart.web.resources;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.launch.JobLauncher;
 
@@ -10,6 +11,9 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Map;
+
+import static com.google.common.collect.ImmutableMap.of;
 
 @Path("/matchjob")
 @Produces(MediaType.TEXT_PLAIN)
@@ -26,7 +30,8 @@ public class MatchJobResource {
     @GET
     public String matchJob() {
         try {
-            launcher.run(this.job, new JobParameters());
+            Map<String, JobParameter> map = of("retry", new JobParameter(System.currentTimeMillis()));
+            launcher.run(this.job, new JobParameters(map));
             return "Succeed to run job";
         } catch (Exception e) {
             logger.error("fail to run job", e);
